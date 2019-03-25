0d4bcfd967b306f48a3f0905ce853d2b7523f756
Kevin Li <kevinli4321@gmail.com>
2019-03-25 14:36:56 -0400

    Initial calculator files

---
+++ Calculator.java
@@ -0,0 +1,7 @@
+public class Calculator {
+  public static double eval(String s) {
+    
+
+    return 1.0;
+  }
+}
--- Driver.java
+++ Driver.java
@@ -1,20 +1,5 @@
 public class Driver {
   public static void main(String[] args) {
-    MyDeque<Integer> rofl = new MyDeque<Integer>();
-
-    rofl.addFirst(69);
-    rofl.addFirst(70);
-    rofl.addFirst(1);
-    rofl.addFirst(1);
-    rofl.addFirst(1);
-    rofl.addFirst(1);
-    rofl.addFirst(1);
-    rofl.addFirst(1);
-    rofl.addFirst(1);
-    rofl.addFirst(1);
-    rofl.addFirst(3);
-    System.out.println("Size: " + rofl.size());
-    System.out.println(rofl);
-    System.out.println(rofl.toStringDebug());
+    
   }
 }
