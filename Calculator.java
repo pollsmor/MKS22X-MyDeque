d6cca2467b5cbe90ee8499b16f0d986fc03a131c
Kevin Li <kevinli4321@gmail.com>
2019-03-25 14:48:59 -0400

    Read in the string into a usable data format

--- Calculator.java
+++ Calculator.java
@@ -1,6 +1,21 @@
 public class Calculator {
   public static double eval(String s) {
-    
+    String[] elements = s.split(" ");
+    char[] operators = new char[elements.length / 2]; //e.g. 9 elements, 4 are operators
+    double[] nums = new double[elements.length - operators.length]; // 9 - 4 = 5;
+    int opsIdx = 0;
+    int numsIdx = 0;
+
+    for (int i = 0; i < elements.length; ++i) {
+      try {
+        nums[numsIdx] = Double.parseDouble(elements[i]);
+        ++numsIdx;
+      }
+
+      catch (NumberFormatException e) {
+        operators[opsIdx] = elements[i].charAt(0);
+      }
+    }
 
     return 1.0;
   }
